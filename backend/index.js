7f6422429173992a00fdac9e186d9d7405b010fc
mahfuzhasanreza <mahfuzhasanreza1@gmail.com>
2024-11-01 22:04:32 +0600

    setup server listen port

--- backend/index.js
+++ backend/index.js
@@ -1,2 +1,13 @@
-const ecpress = require('express') // method 1
-import express from "express";
+// const express = require('express') // method 1
+import express from "express"; // method 2
+import dotenv from "dotenv";
+
+dotenv.config({});
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server listen at port ${PORT}`);
+});
